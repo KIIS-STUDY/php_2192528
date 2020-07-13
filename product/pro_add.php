b49942214d6a35bedd245f2cbb1574854baaade1
Yousyo_ryuu <ryuu9811@icloud.com>
2020-07-13 17:39:29 +0900

    07/13に作成した

---
+++ product/pro_add.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
+
+商品追加<br>
+<br/>
+<form method="post"action="pro_add_check.php">
+商品名を入力してください。<br/>
+<input type="text"name="name"style="width:200px"><br/>
+価額を入力してください。<br/>
+<input type="test"name="price"style="width:50px"><br/>
+<br/>
+<input type="button"onclick="history.back()"value="戻る">
+<input type="submit"value="OK">
+</form>
+
+</body>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
